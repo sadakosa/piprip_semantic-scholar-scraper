c09bfb6f78004ae33ab9c12ad13c5fb9775edc96
sadakosa <sabrinachowzbl@gmail.com>
2024-07-01 23:35:45 +0800

    sql and logger

---
+++ db.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Papers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    paper_id TEXT NOT NULL UNIQUE,
+    corpus_id TEXT,
+    title TEXT NOT NULL,
+    author TEXT,
+    publication_date TEXT,
+    abstract TEXT NOT NULL,
+    url TEXT 
+);
+
+CREATE TABLE References (
+    paper_id TEXT NOT NULL,
+    reference_id TEXT NOT NULL,
+    constraint fk_paper_id foreign key (paper_id) references Papers(paper_id),
+    constraint fk_reference_id foreign key (reference_id) references Papers(paper_id),
+    PRIMARY KEY (paper_id, reference_id)
+);
---
+++ logger/logger.py
@@ -0,0 +1,18 @@
+import os
+import datetime
+
+class Logger: # to store what was the latest successful URL (after saved in SQL)
+    def __init__(self):
+        self.log_dir = "logger/logs"
+        self.log_file = self.get_log_file_name()
+        self.file = open(self.log_file, 'a')
+
+    def get_log_file_name(self):
+        current_datetime = datetime.datetime.now()
+        return os.path.join(self.log_dir, f"trial_{current_datetime.strftime('%Y-%m-%d_%H-%M-%S')}.txt")
+
+    def log_message(self, message):
+        self.file.write(message + '\n')
+
+    def close_log_file(self):
+        self.file.close()
